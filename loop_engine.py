714cc85b3ce425b24dc0fa0cccd0332665e6d1c5
minoru-jp <contactminoru@gmail.com>
2025-06-28 21:42:59 +0900

    modified

--- loop_engine.py
+++ loop_engine.py
@@ -331,14 +331,16 @@ def make_loop_engine_handle(role: str = 'loop', logger = None) -> LoopEngineHand
             raise HandlerError(current, e)
     '''
 
-    _INVOKE_HANDLER_TEMPLATE = '''\
+    #Note: zero indent
+    _INVOKE_HANDLER_TEMPLATE = textwrap.dedent('''\
     current = '{event}'
     {ctx_update}
     result = {await_}{event}(ctx)
     result_setter.set_prev(current, result)
-    '''
+    ''')
 
-    _PAUSABLE_TEMPLATE = '''\
+    #Note: zero indent
+    _PAUSABLE_TEMPLATE = textwrap.dedent('''\
     if running_manager.enter_if_pending_pause():
         {on_pause}
         try:
@@ -356,7 +358,7 @@ def make_loop_engine_handle(role: str = 'loop', logger = None) -> LoopEngineHand
         await running_event.wait()
     except Exception as e:
         raise CircuitError(e)
-    '''
+    ''')
 
     HANDLER_IN_CIRCUIT = {
         'should_stop',
@@ -388,17 +390,19 @@ def make_loop_engine_handle(role: str = 'loop', logger = None) -> LoopEngineHand
                 continue
             async_func = inspect.iscoroutinefunction(handler)
             includes_async_function |= async_func
-            invoking_parts[event] = _INVOKE_HANDLER_TEMPLATE.format(
+            invoking_parts[event] = textwrap.indent(_INVOKE_HANDLER_TEMPLATE.format(
                 event = event,
                 ctx_update = f'ctx_updater("{event}")' if notify_ctx else '',
                 await_ = 'await ' if async_func else '',
+            ),
+            ' ' * 16
             )
             ns_for_handlers[event] = handler
         
-        pause_code = _PAUSABLE_TEMPLATE.format(
+        pause_code = textwrap.indent(_PAUSABLE_TEMPLATE.format(
             on_pause = invoking_parts.get('on_pause', ''),
             on_resume = invoking_parts.get('on_resume', '')
-        ) if pausable else ''
+        ), ' ' * 16) if pausable else ''
 
         full_code = textwrap.dedent(_CIRCUIT_TEMPLATE.format(
             async_ = 'async ' if includes_async_function else '',
