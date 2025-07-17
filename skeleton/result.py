554c24fe593405a9029955ceee087f33e2e26204
minoru-jp <contactminoru@gmail.com>
2025-07-17 17:56:45 +0900

    WIP

--- skeleton/result.py
+++ skeleton/result.py
@@ -5,6 +5,10 @@ from typing import Any
 
 class LastProcessReader(Protocol):
     @property
+    def NO_RECORDED(_) -> object:
+        ...
+
+    @property
     def recorded_last_process(_) -> str:
         ...
     
@@ -13,6 +17,10 @@ class LastProcessReader(Protocol):
         ...
 
 class ResultReader(Protocol):
+    @property
+    def NO_RESULT(_) -> object:
+        ...
+        
     @property
     def result(_) -> Any:
         ...
@@ -26,17 +34,24 @@ class ResultReader(Protocol):
         ...
 
     @property
-    def subroutine(_) -> LastProcessReader:
+    def process(_) -> LastProcessReader:
         ...
 
 class ResultFull(Protocol):
     @staticmethod
-    def get_result_reader(
-        result: Any,
-        outcome: str,
-        last_recorded_process: Any,
-        last_recorded_result: Any,
-    ) -> ResultReader:
+    def set_recorded_last_process(name: str) -> None:
+        ...
+    
+    @staticmethod
+    def set_recorded_last_result(obj: Any) -> None:
+        ...
+    
+    @staticmethod
+    def set_result(outcome: str, obj: Any) -> None:
+        ...
+    
+    @staticmethod
+    def get_result_reader() -> ResultReader:
         ...
 
 def setup_ResultFull() -> ResultFull:
@@ -45,23 +60,42 @@ def setup_ResultFull() -> ResultFull:
         __slots__ = ()
         def __repr__(self):
             return "no result"
+    
+    class _NoRecoded:
+        __slots__ = ()
+        def __repr__(self):
+            return "no recoded"
         
     _NO_RESULT = _NoResult()
+    _NO_RECORDED = _NoRecoded()
 
     _result = _NO_RESULT
     _outcome = str(_NO_RESULT)
     _error = None
 
+    _last_recorded_process = str(_NO_RECORDED)
+    _last_recorded_result = _NO_RECORDED
+
     class _LastProcessReader(LastProcessReader):
         @property
+        def NO_RECORDED(_) -> object:
+            return _NO_RECORDED
+        
+        @property
         def recorded_last_process(_) -> str:
-            ...
+            return _last_recorded_process
     
         @property
         def recorded_last_result(_) -> Any:
-            ...
+            return _last_recorded_result
+    
+    _last_process_reader = _LastProcessReader()
 
-    class _ReaderInterface(ResultReader):
+    class _ResultReaderInterface(ResultReader):
+        @property
+        def NO_RESULT(_) -> object:
+            return _NO_RESULT
+        
         @property
         def result(_) -> Any:
             return _result
@@ -75,18 +109,31 @@ def setup_ResultFull() -> ResultFull:
             return _error
 
         @property
-        def subroutine(_) -> LastProcessReader:
-            ...
-        
-    class _Interface(ResultFull):
-        @staticmethod
-        def get_result_reader(
-            result: Any,
-            outcome: str,
-            last_recorded_process: Any,
-            last_recorded_result: Any,
-        ) -> ResultReader:
-            ...
+        def process(_) -> LastProcessReader:
+            return _last_process_reader
+    
+    _result_reader = _ResultReaderInterface()
 
-    return _Interface()
+    class _Root(ResultFull):
+        @staticmethod
+        def set_recorded_last_process(name: str) -> None:
+            nonlocal _last_recorded_process
+            _last_recorded_process = name
+        
+        @staticmethod
+        def set_recorded_last_result(obj: Any) -> None:
+            nonlocal _last_recorded_result
+            _last_recorded_result = obj
+        
+        @staticmethod
+        def set_result(outcome: str, obj: Any) -> None:
+            nonlocal _outcome, _result
+            _outcome = outcome
+            _result = obj
+        
+        @staticmethod
+        def get_result_reader() -> ResultReader:
+            return _result_reader
+
+    return _Root()
 
