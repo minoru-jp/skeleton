8513e468b366f790ac7069d8d92f6813b181ea22
minoru-jp <contactminoru@gmail.com>
2025-07-19 01:38:40 +0900

    Support both asynchronous and synchronous functions

--- skeleton/routine.py
+++ skeleton/routine.py
@@ -1,10 +1,12 @@
 
+from __future__ import annotations
 
-from typing import Generic, Protocol, runtime_checkable
+from typing import TYPE_CHECKING, Any, Generic, Protocol, runtime_checkable
 
-from .context import Context, T
+if TYPE_CHECKING:
+    from .context import Context, T
 
 @runtime_checkable
 class Routine(Protocol, Generic[T]):
-    async def __call__(self, context: Context[T]):
+    def __call__(self, context: Context[T]) -> Any:
         ...
